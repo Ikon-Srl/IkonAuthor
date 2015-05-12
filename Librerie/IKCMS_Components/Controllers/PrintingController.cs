b65757e81df098ed212115e00935c7592464d699
marcovenuti <marco@ikon.it>
2015-05-12 15:12:15 +0200

    library dependencies

--- Librerie/IKCMS_Components/Controllers/PrintingController.cs
+++ Librerie/IKCMS_Components/Controllers/PrintingController.cs
@@ -29,7 +29,6 @@ using Ikon.IKGD.Library.Resources;
 
 namespace IkonWeb.Controllers
 {
-  using Winnovative.WnvHtmlConvert;
 
 
   [RobotsDeny()]
@@ -37,62 +36,21 @@ namespace IkonWeb.Controllers
   {
 
 
-    //
-    // stampa pagina come PDF
-    //
     public ActionResult RenderAsPDF(string url, int? render_delay)
     {
-      byte[] pdfBytes = null;
-      try
-      {
-      }
-      catch { }
       return null;
     }
 
 
-    //
-    // stampa pagina come PDF
-    //
     public static byte[] RenderAsPDF_Worker(string url, int? render_delay, int? screenWidthPx, bool? dump_html)
     {
-      if (string.IsNullOrEmpty(url) || Utility.CheckIfBOT())
-        return null;
-      try
-      {
-      }
-      //catch (Exception ex)
-      //{
-      //  Elmah.ErrorSignal.FromCurrentContext().Raise(ex);
-      //}
-      catch { }
       return null;
     }
 
 
-    //
-    // stampa pagina come PDF
-    // per abilitare la modalita' IE9 invece di IE7
-    // http://stackoverflow.com/questions/10984599/browser-emulation-using-winnovative-html-to-pdf
-    // regkeys:
-    // DWORD value, Name=WebDev.WebServer.exe, Value=9000 (decimal)
-    // DWORD value, Name=w3wp.exe, Value=9000 (decimal)
-    // HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer\MAIN\FeatureControl\FEATURE_BROWSER_EMULATION
-    // HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Internet Explorer\MAIN\FeatureControl\FEATURE_BROWSER_EMULATION
-    //
     public static byte[] RenderAsPDF_Helper(string html, string baseUrl, int? screenWidthPx, int? render_delay)
     {
-      byte[] pdfBytes = null;
-      try
-      {
-
-      }
-      //catch (Exception ex)
-      //{
-      //  Elmah.ErrorSignal.FromCurrentContext().Raise(ex);
-      //}
-      catch { }
-      return pdfBytes;
+      return null;
     }
 
 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # IkonAuthor
 Ikon Author CMS
+
+Requirements:
+
+
